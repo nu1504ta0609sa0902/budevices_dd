c71683a21c93b0d336dfc3be490a2554b3d018da
TPD_Auto <nooruddin1504@aol.com>
2016-12-20 11:42:47 +0000

    Added -Dcurrent.browser so Jenkins can use it

--- src/test/java/com/mhra/mdcm/devices/dd/appian/utils/driver/BrowserConfig.java
+++ src/test/java/com/mhra/mdcm/devices/dd/appian/utils/driver/BrowserConfig.java
@@ -24,6 +24,7 @@ public class BrowserConfig {
     public WebDriver getDriver() {
 
         if (browser == null) {
+            //Should be picked up
             browser = System.getProperty("current.browser");
         }
 
